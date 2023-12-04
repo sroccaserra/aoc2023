fc1ddb606eb3baecef940981852ca0896ce450fd
sroccaserra <sroccaserra@yahoo.com>
2023-12-04 09:33:33 +0100

    f04 Solve part two

--- 04.py
+++ 04.py
@@ -1,4 +1,5 @@
 from re import split
+from collections import deque
 
 
 def solve_1(cards):
@@ -13,6 +14,21 @@ def solve_1(cards):
     return result
 
 
+def solve_2(cards):
+    result = len(cards)
+    indexed = [(i, v) for i,v in enumerate(cards)]
+    q = deque(indexed)
+    while q:
+        index, card = q.popleft()
+        k = index
+        for have in card[1]:
+            if have in card[0]:
+                k = k+1
+                q.append(indexed[k])
+                result += 1
+    return result
+
+
 ex = """Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -23,7 +39,8 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
 
 
 file = ex
-file = open(0).read()
+# file = open(0).read()
 lines = file[:-1].split('\n')
 cards = [[part.split() for part in split(r': | \| ', line)[1:]] for line in lines]
 print(solve_1(cards))
+print(solve_2(cards))
