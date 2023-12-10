d59af250d967c9644c22be8c9f42c660bc6b8d9f
sroccaserra <sroccaserra@yahoo.com>
2023-12-10 20:45:33 +0100

    f10 Solve part two

--- 10.py
+++ 10.py
@@ -22,6 +22,41 @@ def solve_1(grid):
     return result
 
 
+def solve_2(grid):
+    start = None
+    for k, v in grid.items():
+        if v == 'S':
+            start = k
+            break
+    dists = {start: 0}
+    s = [start]
+    polygon = [start]
+    while s:
+        pos = s.pop()
+        d = dists[pos]
+        first_found = False
+        for n in neighbors(grid, pos):
+            if first_found:
+                continue
+            if n in dists:
+                continue
+            first_found = True
+            s.append(n)
+            polygon.append(n)
+            dists[n] = d+1
+    for k, v in grid.items():
+        if k not in dists:
+            grid[k] = '.'
+        else:
+            grid[k] = str(dists[k]%10)
+    polygon.append(start)
+    result = 0
+    for k, v in grid.items():
+        if v == '.' and is_inside(polygon, k):
+            result += 1
+    return result
+
+
 def neighbors(grid, pos):
     i, j = pos
     result = []
@@ -37,11 +72,37 @@ def neighbors(grid, pos):
     return result
 
 
+def is_inside(polygon, point):
+    x, y = point
+    nb_right = 0
+    for i in range(len(polygon)-1):
+        x1, y1 = polygon[i]
+        x2, y2 = polygon[i+1]
+        if y1 == y2:
+            continue
+        assert x1 == x2
+        if min(y1, y2) <= y < max(y1, y2):
+            assert x != x1
+            if x < x1:
+                nb_right += 1
+    return nb_right %2 == 1
+
+
 ex = """-L|F7
 7S-7|
 L|7||
 -L-J|
 L|-JF
+"""
+ex = """...........
+.S-------7.
+.|F-----7|.
+.||.....||.
+.||.....||.
+.|L-7.F-J|.
+.|..|.|..|.
+.L--J.L--J.
+...........
 """
 text = ex
 # text = open(0).read()
@@ -54,3 +115,11 @@ for i in range(h):
     for j in range(w):
         grid[(i, j)] = line[j]
 print(solve_1(grid))
+print(solve_2(grid))
+
+exit(0)
+for i in range(h):
+    line = lines[i]
+    for j in range(w):
+        print(grid[(i, j)], end='')
+    print()
