4425e51e736c89b35685202d403285b5bbb8ea59
sroccaserra <sroccaserra@yahoo.com>
2023-12-13 09:06:59 +0100

    r13 Factorize solutions

--- 13.py
+++ 13.py
@@ -1,8 +1,8 @@
-def solve_1(patterns):
+def solve(expected_diff, patterns):
     result_h = 0
     result_v = 0
     for p in patterns:
-        result_h += count_horizontal(p)
+        result_h += count_with_diff_horizontally(expected_diff, p)
         w = len(p[0])
         h = len(p)
         transposed = []
@@ -11,45 +11,18 @@ def solve_1(patterns):
             transposed.append(row)
             for i in range(h):
                 row.append(p[i][j])
-        result_v += count_horizontal(transposed)
+        result_v += count_with_diff_horizontally(expected_diff, transposed)
 
     return result_h*100+result_v
 
-def solve_2(patterns):
-    result_h = 0
-    result_v = 0
-    for p in patterns:
-        result_h += find_with_diff_of_one(p)
-        w = len(p[0])
-        h = len(p)
-        transposed = []
-        for j in range(w):
-            row = []
-            transposed.append(row)
-            for i in range(h):
-                row.append(p[i][j])
-        result_v += find_with_diff_of_one(transposed)
 
-    return result_h*100+result_v
-
-def count_horizontal(p):
+def count_with_diff_horizontally(expected_diff, pattern):
     n = 0
-    for n in range(len(p)-1):
-        a = min(n+1, len(p)-n-1)
+    for n in range(len(pattern)-1):
+        a = min(n+1, len(pattern)-n-1)
         start = n+1-a
         end = n+1+a
-        sub = p[start:end]
-        if sub == list(reversed(sub)):
-            return n+1
-    return 0
-
-def find_with_diff_of_one(p):
-    n = 0
-    for n in range(len(p)-1):
-        a = min(n+1, len(p)-n-1)
-        start = n+1-a
-        end = n+1+a
-        sub = p[start:end]
+        sub = pattern[start:end]
         w = len(sub[0])
         i = 0
         diff = 0
@@ -59,9 +32,10 @@ def find_with_diff_of_one(p):
                 if line[j] != line_r[j]:
                     diff += 1
             i += 1
-        if diff == 2:
+        if diff == 2*expected_diff:
             return n+1
     return 0
+
 
 ex = """#.##..##.
 ..#.##.#.
@@ -80,7 +54,7 @@ ex = """#.##..##.
 #....#..#
 """
 text = ex
-# text = open(0).read()
+text = open(0).read()
 patterns = [lines.split('\n') for lines in text[:-1].split('\n\n')]
-print(solve_1(patterns))
-print(solve_2(patterns))
+print(solve(0, patterns))
+print(solve(1, patterns))
