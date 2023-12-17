1eddaf893ddb3c2577b60788565ad349389dd34e
sroccaserra <sroccaserra@yahoo.com>
2023-12-17 15:49:37 +0100

    f17 Solve part two

--- 17.py
+++ 17.py
@@ -1,27 +1,60 @@
 from collections import deque
 
 
+HUGE = 999999999
+MIN_TO_STOP = 4
+
+
 def solve_1(matrix):
+    return solve(matrix, neighbors)
+
+
+def solve_2(matrix):
+    return solve(matrix, ultra_neighbors, 4)
+
+
+def solve(matrix, neighbors_fn, min_to_stop = 0):
     w = len(matrix[0])
     h = len(matrix)
     start_1 = ((0, 0, 0, 1, 0), 0)  # ((i, j, di, dj, n), h)
     start_2 = ((0, 0, 1, 0, 0), 0)  # ((i, j, di, dj, n), h)
-    seen = {start_1: 99999, start_2: 9999999}
+    seen = {start_1: HUGE, start_2: HUGE}
     q = deque([start_1, start_2])
     while q:
         node, heat = q.popleft()
-        for n in neighbors(w, h, node):
+        for n in neighbors_fn(w, h, node):
             ni = n[0]
             nj = n[1]
             new_heat = heat + matrix[ni][nj]
-            if n not in seen or (n in seen and new_heat < seen[n]):
+            if n in seen and seen[n] <= new_heat:
+                continue
+            if (not at_end(w, h, n) or min_to_stop <= n[4]):
                 seen[n] = new_heat
                 q.append((n, new_heat))
-    result = 99999
-    for k, v in seen.items():
-        if k[0] == h-1 and k[1] == w-1:
-            result = min(result, v)
-    return result
+    return min([v for k, v in seen.items() if at_end(w, h, k)])
+
+
+def at_end(w, h, n):
+    return n[0] == h-1 and n[1] == w-1
+
+
+def is_in(w, h, i, j):
+    return (0 <= i < h) and (0 <= j < w)
+
+
+def left(di, dj):
+    assert 0 == di or 0 == dj
+    if di == 0:
+        return (-1, 0) if dj == 1 else (1, 0)
+    else:
+        return (0, 1) if di == 1 else (0, -1)
+
+
+def right(di, dj):
+    if di == 0:
+        return (1, 0) if dj == 1 else (-1, 0)
+    else:
+        return (0, -1) if di == 1 else (0, 1)
 
 
 def neighbors(w, h, node):
@@ -42,32 +75,27 @@ def neighbors(w, h, node):
     return result
 
 
-def is_in(w, h, i, j):
-    return (0 <= i < h) and (0 <= j < w)
+def ultra_neighbors(w, h, node):
+    i, j, di, dj, n = node
+    if n < 4 and is_in(w, h, i+di, j+dj):
+        return [(i+di, j+dj, di, dj, n+1)]
 
-
-def left(di, dj):
-    assert 0 == di or 0 == dj
-    if di == 0:
-        return (-1, 0) if dj == 1 else (1, 0)
-    else:
-        return (0, 1) if di == 1 else (0, -1)
-
-assert (-1, 0) == left(0, 1)
-assert (1, 0) == left(0, -1)
-assert (0, 1) == left(1, 0)
-assert (0, -1) == left(-1, 0)
-
-def right(di, dj):
-    if di == 0:
-        return (1, 0) if dj == 1 else (-1, 0)
-    else:
-        return (0, -1) if di == 1 else (0, 1)
-
-assert (1, 0) == right(0, 1)
-assert (-1, 0) == right(0, -1)
-assert (0, -1) == right(1, 0)
-assert (0, 1) == right(-1, 0)
+    result = []
+    if n < 10 and is_in(w, h, i+di, j+dj):
+        result.append((i+di, j+dj, di, dj, n+1))
+    can_turn = ((0 < i < h-1) and (0 < j < w-1)) or MIN_TO_STOP <= n
+    if can_turn:
+        left_di, left_dj = left(di, dj)
+        left_i = i+left_di
+        left_j = j+left_dj
+        if is_in(w, h, left_i, left_j):
+            result.append((left_i, left_j, left_di, left_dj, 1))
+        right_di, right_dj = right(di, dj)
+        right_i = i+right_di
+        right_j = j+right_dj
+        if is_in(w, h, right_i, right_j):
+            result.append((right_i, right_j, right_di, right_dj, 1))
+    return result
 
 
 ex = """2413432311323
@@ -88,3 +116,4 @@ text = ex
 # text = open(0).read()
 numbers = [[int(c) for c in line] for line in text.splitlines()]
 print(solve_1(numbers))
+print(solve_2(numbers))
