c4e575fe2c712dc58c2685c56b22526afa1ee761
Sébastien Roccaserra <sroccaserra@yahoo.com>
2023-12-20 15:17:37 +0100

    r18 Use trapezoid formula for both parts

--- 18.py
+++ 18.py
@@ -1,46 +1,25 @@
-from collections import deque
+def solve(read_polygon_fn, instructions):
+    polygon = read_polygon_fn(instructions)
+    return cube_meters(polygon)
 
 
-def solve_1(instructions):
+def read_polygon_1(instructions):
     x, y = 0,0
     polygon = [(x, y)]
-    seen = {(x, y)}
     for d, n, _ in instructions:
         if d == 'U':
-            for k in range(n):
-                seen.add((x, y - k))
             y -= n
         if d == 'D':
-            for k in range(n):
-                seen.add((x, y + k))
             y += n
         if d == 'L':
-            for k in range(n):
-                seen.add((x - k, y))
             x -= n
         if d == 'R':
-            for k in range(n):
-                seen.add((x + k, y))
             x += n
         polygon.append((x, y))
-        seen.add((x, y))
-    flood_fill(seen, 1, 1)
-    return len(seen)
+    return polygon
 
 
-def solve_2(instructions):
-    polygon = read_polygon(instructions)
-    length = 0
-    area = 0
-    for i in range(len(polygon)-1):
-        x1, y1 = polygon[i]
-        x2, y2 = polygon[i+1]
-        area += y1*(x1-x2)  # trapezoid formula: sum of ½(y1+y2)(x1-x2)
-        length += (abs(x2-x1) + abs(y2-y1))
-    return area+length//2+1
-
-
-def read_polygon(isntructions):
+def read_polygon_2(isntructions):
     x, y = 0, 0
     polygon:list[tuple] = [(0, 0)]
     for _, _, h in instructions:
@@ -58,15 +37,16 @@ def read_polygon(isntructions):
     return polygon
 
 
-def flood_fill(seen, x, y):
-    q = deque([(x, y)])
-    while q:
-        x, y = q.popleft()
-        for dy in [-1,0, 1]:
-            for dx in [-1,0, 1]:
-                if (x+dx, y+dy) not in seen:
-                    q.append((x+dx, y+dy))
-                    seen.add((x+dx, y+dy))
+def cube_meters(polygon):
+    length, area = 0, 0
+    for i in range(len(polygon)-1):
+        x1, y1 = polygon[i]
+        x2, y2 = polygon[i+1]
+        area += y1*(x1-x2)  # trapezoid formula: sum of ½(y1+y2)(x1-x2)
+        length += abs(x2-x1) + abs(y2-y1)
+    # `length//2 - 1` cube meters are already included in area =>
+    # to count the full `length` cube metters, we add `length//2 + 1`
+    return area + length//2 + 1
 
 
 def parse(line):
@@ -92,5 +72,5 @@ U 2 (#7a21e3)
 text = ex
 # text = open(0).read()
 instructions = [parse(line) for line in text.splitlines()]
-print(solve_1(instructions))
-print(solve_2(instructions))
+print(solve(read_polygon_1, instructions))
+print(solve(read_polygon_2, instructions))
